781daaafdc8b18ca1a46d7ec5e868b110b2f9863
KJ Price <kjprice12@gmail.com>
2024-07-20 07:39:35 -0500

    Create skeleton of commands

--- directories.py
+++ directories.py
@@ -1,1 +1,64 @@
 #!/usr/bin/env python3
+
+_HELP = """
+Valid commands:
+ - CREATE path/name
+ - DELETE path/name
+ - MOVE path1/name path2/name
+ - LIST
+ - HELP
+"""
+
+
+def create(path: str) -> None:
+    pass
+
+
+def delete(path: str) -> None:
+    pass
+
+
+def move(path1: str, path2: str) -> None:
+    pass
+
+
+def list() -> None:
+    pass
+
+
+def help() -> None:
+    print(_HELP)
+
+
+_COMMANDS = {
+    "create": create,
+    "delete": delete,
+    "move": move,
+    "list": list,
+    "help": help,
+}
+
+
+def main():
+    print(_HELP)
+
+    while True:
+        full_command = input("")
+        command = None
+        args = []
+        if not " " in full_command:
+            command = full_command.lower()
+        else:
+            command, _, _args = full_command.partition(" ")
+            command = command.lower()
+            args = _args.split(" ")
+
+        if not command in _COMMANDS:
+            print("Invalid command.")
+            print(_HELP)
+        else:
+            _COMMANDS[command](*args)
+
+
+if __name__ == "__main__":
+    main()
